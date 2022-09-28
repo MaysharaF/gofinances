c92ff6e6c5fc39c09b2eaba727471e2a7ee5bfc4
MaysharaF <mayshara@alunos.utfpr.edu.br>
2022-09-28 20:25:48 -0300

    feat: criando header do dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  Photo,
+  User,
+  UserGreeting,
+  UserInfo,
+  UserName,
+  UserWrapper,
+  Icon,
+} from "./styles";
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/43793682?v=4",
+              }}
+            />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Mayshara</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 };
